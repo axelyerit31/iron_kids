d24ae120beca4455534eba2518ea6591a0311e81
Josue MarDi <originaljosue@gmail.com>
2023-03-07 21:16:00 -0500

    alguna mejora

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:iron_kids/styles/appTheme.dart';
+import 'package:iron_kids/styles/widgets.dart';
 
 final TextEditingController _emailController = TextEditingController();
 final TextEditingController _passwordController = TextEditingController();
@@ -69,42 +70,6 @@ class Formlogin extends StatelessWidget {
           AppTheme.spacingWidget5,
           const ButtonUI(text: 'Inicia sesión'),
         ],
-      ),
-    );
-  }
-}
-
-//Widget de los Inputs
-class InputField extends StatelessWidget {
-  TextEditingController controller = TextEditingController();
-  final String placeholder;
-  InputField({
-    super.key,
-    required this.controller,
-    required this.placeholder,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 44,
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        color: AppTheme.gray50,
-        borderRadius: AppTheme.borderRadiusL,
-        border: Border.all(
-          color: AppTheme.gray300,
-          width: 1.0,
-        ),
-      ),
-      child: TextField(
-        controller: controller,
-        decoration: InputDecoration(
-            hintText: placeholder,
-            border: InputBorder.none,
-            hintStyle: AppTheme.labelLarge(context)),
-        obscureText: true,
       ),
     );
   }
--- lib/styles/widgets.dart
+++ lib/styles/widgets.dart
@@ -1,2 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:iron_kids/styles/appTheme.dart';
+
 // botones
 // inputs
+
+//Widget de los Inputs
+
+class InputField extends StatelessWidget {
+  TextEditingController controller = TextEditingController();
+  final String placeholder;
+  InputField({
+    super.key,
+    required this.controller,
+    required this.placeholder,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 44,
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: AppTheme.gray50,
+        borderRadius: AppTheme.borderRadiusL,
+        border: Border.all(
+          color: AppTheme.gray300,
+          width: 1.0,
+        ),
+      ),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+            hintText: placeholder,
+            border: InputBorder.none,
+            hintStyle: AppTheme.labelLarge(context)),
+        obscureText: true,
+      ),
+    );
+  }
+}
