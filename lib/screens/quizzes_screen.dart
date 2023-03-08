fa99cec4006465b35f9d14153cd05f0165dedd7a
Josue MarDi <originaljosue@gmail.com>
2023-03-08 18:05:22 -0500

    nueva pantalla 'quizzes'

---
+++ lib/screens/quizzes_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QuizzesScreen extends StatelessWidget {
+  const QuizzesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
